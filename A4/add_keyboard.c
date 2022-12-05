ace0a19a55f2fbe38da6ac947019976d97ca5953
root <root@lfs-lmalki-h.org>
2022-12-05 15:32:09 +0100

    working on A4

---
+++ A4/add_keyboard.c
@@ -0,0 +1,18 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("lmalki-h");
+
+static int __init add_keyboard_init(void) {
+ pr_info("USB keyboard connected!\n");
+ return 0;
+}
+
+static void __exit add_keyboard_exit(void) {
+ pr_info("Cleaning up module\n");
+}
+
+module_init(add_keyboard_init);
+module_exit(add_keyboard_exit);
