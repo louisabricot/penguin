a2304030b5ce3398f067db38f6db5b30f7db2e0a
root <root@lfs-lmalki-h.org>
2022-12-08 12:29:53 +0100

    normed

--- A4/add_keyboard.c
+++ A4/add_keyboard.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0 OR MIT
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -5,13 +7,15 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("lmalki-h");
 
-static int __init add_keyboard_init(void) {
- pr_info("USB keyboard connected! YOUHOU\n");
- return 0;
+static int __init add_keyboard_init(void)
+{
+	pr_info("USB keyboard connected! YOUHOU\n");
+	return 0;
 }
 
-static void __exit add_keyboard_exit(void) {
- pr_info("Cleaning up module\n");
+static void __exit add_keyboard_exit(void)
+{
+	pr_info("Cleaning up module\n");
 }
 
 module_init(add_keyboard_init);
