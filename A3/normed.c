4e0c85af2fb455248175354d982550f457f1a170
Louisa Malki-Haegel <malkilouisa@protonmail.com>
2023-03-21 18:33:47 +0100

    normed using checkpatch.pl

--- A3/normed.c
+++ A3/normed.c
@@ -1,26 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0 OR MIT
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/delay.h>
 #include <linux/slab.h>
 
+MODULE_LICENSE("GPL");
+
 void do_work(int *n)
 {
-        int i;
+	int i;
 
-        for (i = 0; i < n; ++i)
-                udelay(10);
+	for (i = 0; i < n; ++i)
+		udelay(10);
 
-        if (*n < 10)
-                /* That was a long sleep, tell userspace about it */
-                pr_info("We slept a long time!");
+	if (*n < 10)
+		/* That was a long sleep, tell userspace about it */
+		pr_info("We slept a long time!");
 }
 
 int my_init(void)
 {
-        int n = 10;
+	int n = 10;
 
-        do_work(&n);
-        return 0;
+	do_work(&n);
+	return 0;
 }
 
 void my_exit(void)
