4a52a56c63934cee85b89c3963ea940e3a6e2ff8
Louisa Malki-Haegel <malkilouisa@protonmail.com>
2023-03-21 18:33:45 +0100

    First commit !

---
+++ A3/normed.c
@@ -0,0 +1,31 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/delay.h>
+#include <linux/slab.h>
+
+void do_work(int *n)
+{
+        int i;
+
+        for (i = 0; i < n; ++i)
+                udelay(10);
+
+        if (*n < 10)
+                /* That was a long sleep, tell userspace about it */
+                pr_info("We slept a long time!");
+}
+
+int my_init(void)
+{
+        int n = 10;
+
+        do_work(&n);
+        return 0;
+}
+
+void my_exit(void)
+{
+}
+
+module_init(my_init);
+module_exit(my_exit);
